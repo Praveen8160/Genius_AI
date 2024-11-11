05b5e4ea2445a4970d845b942e9e326a776c496e
Praveen8160 <praveenp8160@gmail.com>
2024-11-11 18:18:41 +0530

    fix bugs

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
+import { useAuth } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const { isSignedIn } = useAuth();
+  const router = useRouter();
+  if (isSignedIn) {
+    router.push("/Dashboard");
+  }
   return (
     <div className="h-full">
-      
       <LandingNavbar />
-      <LandingHero/>
+      <LandingHero />
     </div>
   );
 }
